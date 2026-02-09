9bc005e63ac6c39981e1243a180573f6f17b2be8
unknown <jrc.jay38@gmail.com>
2026-02-10 00:09:49 +0400

    New

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import ProjectGrid from './components/ProjectGrid';
-import { Mail, Github, Linkedin } from 'lucide-react';
+import { Mail, Github } from 'lucide-react';
 
 function App() {
   return (
